b9836d040f897641cb5e09519bfc4c835bdd46c5
root <dilesoft@gmail.com>
2021-10-26 10:53:06 +0300

    servers check

--- ws/check.js
+++ ws/check.js
@@ -7,21 +7,31 @@ const port = 2222;
 const servers = require('./servers');
 const status = {};
 
+const changeStatus = (server, newStatus) => {
+  if (!status[server]) {
+    status[server] = {}
+  }
+  if (status[server].status !== newStatus) {
+    status[server].changedTime = new Date().toISOString();
+  }
+  status[server].status = newStatus;
+}
+
 setInterval(() => {
     servers.forEach(server => {
         let client = new net.Socket();
         client.setTimeout(1000);
         client.on('timeout', () => {
-            status[server] = false;
+            changeStatus(server, false);
             client.destroy();
         });
         client.on('error', () => {
-          status[server] = false;
+          changeStatus(server, false);
           client.destroy();
         });
         client.connect(server.endsWith(' [cx]') ? 2195 : 2197, server.endsWith(' [cx]') ? server.match(/^(.*) \[cx\]$/)[1] : server, function() {
             // client.write('Vivat Sicher, Rock\'n\'Roll forever!!!\x00\x01');
-            status[server] = true;
+            changeStatus(server, true);
             client.destroy();
         });
     });
